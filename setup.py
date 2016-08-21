1a4968bcccd872680bdf93276bb940f1f8a2ce9a
Jay Baker <jay@jaybakerweb.com>
2016-08-21 17:19:24 -0500

    Added setup back in for pip.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'gaeutils',
+  packages = ['gaeutils'],
+  version = '0.1',
+  description = 'Simple set of GAE utilities',
+  author = 'Jay Baker',
+  author_email = 'jbaker.work@gmail.com',
+  url = 'https://github.com/jaybaker/gaeutils',
+  download_url = 'https://github.com/jaybaker/gaeutils/tarball/0.1',
+  keywords = ['google', 'app engine', 'gae'],
+  classifiers = [],
+)
