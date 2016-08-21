15cce7201b3315529acf05daa5e4ce7d3568e65f
Jay Baker <jay@jaybakerweb.com>
2016-08-21 16:05:48 -0500

    Prepped for PyPi.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'gaeutils',
+  packages = ['gaeutils'],
+  version = '0.1',
+  description = 'A collection of utilities for Google App Engine',
+  author = 'Jay Baker',
+  author_email = 'jbaker.work@gmail.com',
+  url = 'https://github.com/jaybaker/gaeutils',
+  download_url = 'https://github.com/jaybaker/gaeutils/tarball/0.1',
+  keywords = ['gae', 'google', 'app engine'],
+  classifiers = [],
+)
